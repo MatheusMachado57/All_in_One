053b1e2fcf6cce3c7c77c5b3a2651d0828ea94aa
Matheus Machado de Almeida <40668494+MatheusMachado57@users.noreply.github.com>
2021-09-26 14:37:03 -0300

    PCA

---
+++ PCA/Aula12.R
@@ -0,0 +1,74 @@
+require(readr)
+
+#carregar banco saude.csv
+
+saude <- read_csv2("saude.CSV")
+
+# remover variáveis qualitativas
+saude2 <- saude[,-1]
+
+### Utilizando prcomp() - Primerio método ###########################
+pca <- prcomp(saude2, scale = TRUE) 
+pca
+
+## plot pc1 and pc2
+plot(pca$x[,1], pca$x[,2])
+
+# Construindo scree plot
+screeplot(pca)
+
+### Utilizando pacote MVar.pt - Segundo Método ######################
+
+library(MVar.pt)
+
+PC <- PCA(saude2, type=2) # Executa o PCA
+Titles = c("Grafico das Observacoes", 
+           "Circulo de Correlacoes")
+
+Plot.PCA(PC, Titles)
+
+PC[["mtxAutvlr"]]
+
+### PCA com train() - Terceiro Método ###############################
+
+library(caret)
+
+# Criar amostras treino/teste
+set.seed(100)
+inTrain  <- createDataPartition(y = saude$Sexo, 
+                                p = 0.75, 
+                                list = F)
+training <- saude[ inTrain,]
+testing  <- saude[-inTrain,]
+
+#####
+
+ctrl <- trainControl(method = "boot", 
+                     number = 30, 
+                     preProcOptions = list(thresh = 0.95))
+                   
+modelFit_pca <- train(Sexo ~ .,
+                      data = training, 
+                      method = "knn",
+                      preProcess = "pca", 
+                      trControl = ctrl )
+
+# Detalhes do treinamento
+modelFit_pca
+
+# Detalhes do pre-processamento
+modelFit_pca$preProcess
+
+### PCA com preProcess() - Quarto Método ############################
+
+# padrao: thresh=0.95
+pca <- preProcess(training, 
+                  method = "pca")
+
+training_pca <- predict(pca, 
+                        training)
+
+# alterando thresh
+pca <- preProcess(training, 
+                  method = "pca", 
+                  thresh = 0.8); pca
